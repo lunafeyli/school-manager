d929369594bcb9e365983214f8707608bfdad68c
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2021-11-28 17:53:31 -0300

    :lipstick: configure theme

--- App.tsx
+++ App.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 
 import Routes from "./src/routes";
-import theme from "./src/styles/theme";
+import { getTheme } from "./src/styles/theme";
 
 export default function App() {
 	return (
-		<ThemeProvider theme={theme}>
+		<ThemeProvider theme={getTheme("light")}>
 			<Routes />
 		</ThemeProvider>
 	);
--- src/styles/theme/index.ts
+++ src/styles/theme/index.ts
@@ -1,18 +1,36 @@
-type ThemeTypes = typeof theme;
+type ThemeTypes = typeof themeType;
 
 export { ThemeTypes };
 
-const theme = {
+const themeLight = {
 	colors: {
-		primary: "#ff6ee8",
-		secondary: "#ff7bea",
-		dark: "#131313",
-		darkGray: "#1a1b1f",
-		gray: "#696e77",
-		lightGray: "#a0a2a7",
-		lighterGray: "#fafbff",
-		white: "#ffffff",
+		primary: "#1f3bad",
+		secondary: "#3352cc",
+		lighterGray: "#f5f5f5",
+		lightGray: "#aeb0b7",
+		gray: "#787b87",
+		darkGray: "#454854",
+		white: "#f5f5f5",
+		dark: "#22242a",
 	},
 };
 
-export default theme;
+const themeDark = {
+	colors: {
+		primary: "#758ef0",
+		secondary: "#a8b6f0",
+		lighterGray: "#22242a",
+		lightGray: "#60626c",
+		gray: "#93959f",
+		darkGray: "#cacbce",
+		white: "#22242a",
+		dark: "#f2f2f3",
+	},
+};
+
+const getTheme = (theme: "light" | "dark") =>
+	theme === "light" ? themeLight : themeDark;
+
+const themeType = getTheme("light");
+
+export { getTheme };
